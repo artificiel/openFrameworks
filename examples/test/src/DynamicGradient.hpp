f74fb0561c1828ad11866a8a7ef0a86e7eafa7f0
alexandre burton <burton@artificiel.org>
2022-12-12 18:13:34 -0500

    initial commit

---
+++ examples/test/src/DynamicGradient.hpp
@@ -0,0 +1,12 @@
+//
+//  DynamicGradient.hpp
+//  test
+//
+//  Created by alexandre burton on 2022-09-15.
+//
+
+#ifndef DynamicGradient_h
+#define DynamicGradient_h
+
+
+#endif /* DynamicGradient_h */
